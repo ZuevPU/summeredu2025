2a70b58933c747a2bc441807b47fa2b7a9b189a0
Зуев Петр <zuevpu@Gmail.com>
2025-08-06 00:28:20 +0300

    1

---
+++ moderators.js
@@ -0,0 +1,68 @@
+const masterclasses = [
+    {
+      photo: "sol.jpg",
+      name: "Олег Солдатов",
+      position: "Бас-гитарист",
+      tag: "Мастер-класс",
+      topic: "Жизнь — это не партитура. Как звучит импровизация.",
+      bullets: [
+      ],
+      ppt:"https://disk.yandex.ru/i/pf6ZbNJFX413mQ", notes:""
+    },
+    {
+        photo: "vad.jpg",
+        name: "Вадим Дмитриев",
+        position: "хороший человек, Автор более 200 игри интерактивных форматов",
+        tag: "Мастер-класс",
+        topic: "Разные люди и разные сюжеты",
+        bullets: [
+        ],
+        ppt:"https://disk.yandex.ru/i/kJfh3mUqwT1Z2A", notes:""
+      }
+      ,
+    {
+        photo: "k.jpg",
+        name: "Анастасия Левшина",
+        position: "кандидат психологических наук, доцент ЮФУ, автор сценария и методолог мультсериала «Нейроны»",
+        tag: "Мастер-класс",
+        topic: "Больше, чем мультфильм: как  учить, развлекая и развлекать, обучая",
+        bullets: [
+        ],
+        ppt:"https://disk.yandex.ru/d/bwHKqrRvTTsq2A", notes:""
+      }
+      ,
+    {
+        photo: "kor.jpg",
+        name: "Коричина Юлия",
+        position: "Умеет все",
+        tag: "Мастер-класс",
+        topic: "Легкое движение: искусство танцевальной импровизации- путь к свободному самовыражению.",
+        bullets: [
+        ],
+        ppt:"https://disk.yandex.ru/i/mJM4wPKT9srlhQ", notes:""
+      }
+      ,
+    {
+        photo: "pik.jpg",
+        name: "Дмитрий Пикалов",
+        position: "доцент ГБОУ ВО СГПИ, лектор РОЗ",
+        tag: "Мастер-класс",
+        topic: "Квантовый спектакль в саду расходящихся тропок:  учимся и обучаем в условиях тотальной неопределённости",
+        bullets: [
+        ],
+        ppt:"https://disk.yandex.ru/i/TavRm3WsPoeQpA", notes:""
+      }
+      ,
+    {
+        photo: "nil.jpg",
+        name: "Евгений Нилов",
+        position: "думает о всем",
+        tag: "Мастер-класс",
+        topic: "«Как вести разговор с ИИ? Основы контекст-инжиниринга»",
+        bullets: [
+
+        ],
+        ppt:"https://disk.yandex.ru/i/Xl24xe6dWPn_3w", notes:""
+      }
+  ];
+  
---
+++ winners.js
@@ -0,0 +1,45 @@
+/* winners.js — работы-победители Летнего ПедСовета 2025 */
+
+const winners = [
+    {
+      nomination : "Культурный код",
+      name       : "Рыбальченко Наталья Сергеевна",
+      work       : "«День России»",
+      position   : "участница Летнего ПедСовета",
+      video      : "https://vk.com/wall758594826_671",
+      poster     : "thumbs/rybalchenko.jpg"          // добавьте превью, если нужно
+    },
+    {
+      nomination : "Нейросети в образовании",
+      name       : "Конашенкова Наталья Игоревна",
+      work       : "«Дед, привет! Взял бы в разведку?»",
+      position   : "участница Летнего ПедСовета",
+      video      : "https://disk.yandex.ru/i/VhnRKJf-y9t7og",
+      poster     : "thumbs/konashenkova.jpg"
+    },
+    {
+      nomination : "Социальная коммуникация",
+      name       : "Григорьева Людмила Николаевна",
+      work       : "«Буллинг»",
+      position   : "участница Летнего ПедСовета",
+      video      : "https://disk.yandex.ru/d/5kCqRKNKHCFI4g",
+      poster     : "thumbs/grigorieva.jpg"
+    },
+    {
+      nomination : "Самоопределение подростков",
+      name       : "Скворцова Елена Вячеславовна",
+      work       : "«Вася, давай!»",
+      position   : "участница Летнего ПедСовета",
+      video      : "https://drive.google.com/file/d/1DCpdgWqxyvcW1SegKTC953_6QVWn1huA/view?usp=drive_link",
+      poster     : "thumbs/skvortsova.jpg"
+    },
+    {
+      nomination : "Престиж профессии учителя",
+      name       : "Подкорытова Юлия Валерьевна",
+      work       : "«Я — сельский учитель»",
+      position   : "участница Летнего ПедСовета",
+      video      : "https://vk.com/wall302379721_822",
+      poster     : "thumbs/podkorytova.jpg"
+    }
+  ];
+  
